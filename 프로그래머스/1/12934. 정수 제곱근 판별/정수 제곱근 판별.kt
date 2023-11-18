c03539bdd21f5b2089055e209187618e602ef9ca
JunBum2ya <141423549+JunBum2ya@users.noreply.github.com>
2023-11-18 15:17:18 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 61.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.kt
@@ -0,0 +1,12 @@
+import java.lang.Math
+
+class Solution {
+   fun solution(n: Long): Long {
+        val sqrt = Math.sqrt(n.toDouble())
+        return if(sqrt % 1.0 == 0.0) {
+            Math.pow(sqrt + 1, 2.0).toLong()
+        } else {
+            -1L
+        }
+    }
+}
