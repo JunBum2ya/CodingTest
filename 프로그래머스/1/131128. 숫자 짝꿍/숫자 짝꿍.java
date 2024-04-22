eb27fa53ecdb47e2e72af1d6d616a792d2ecf5e9
Junbum2ya <141423549+JunBum2ya@users.noreply.github.com>
2024-04-22 18:20:38 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.18 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -4,11 +4,11 @@ import java.util.Collections;
 class Solution {
     public String solution(String X, String Y) {
         StringBuilder answer = new StringBuilder();
-        int[] xArray = convertArray(X);
-        int[] yArray = convertArray(Y);
+        Map<Integer,Integer> xMap = convertMap(X);
+        Map<Integer,Integer> yMap = convertMap(Y);
         for(int i = 9; i >= 0; i--) {
-            int xCount = xArray[i];
-            int yCount = yArray[i];
+            int xCount = xMap.getOrDefault(i, 0);
+            int yCount = yMap.getOrDefault(i, 0);
             int count = xCount < yCount ? xCount : yCount;
             for(int j = 0; j < count; j++) {
                 answer.append(i);
@@ -23,12 +23,13 @@ class Solution {
         return answer.toString();
     }
     
-    private int[] convertArray(String numberStr) {
-        int[] array = new int[10];
+    private Map<Integer,Integer> convertMap(String numberStr) {
+        Map<Integer,Integer> map = new HashMap<>();
         for(int i = 0; i < numberStr.length(); i++) {
             int key = numberStr.charAt(i) - '0';
-            array[key]++;
+            int count = map.getOrDefault(key, 0) + 1;
+            map.put(key, count);
         }
-        return array;
+        return map;
     }
 }
