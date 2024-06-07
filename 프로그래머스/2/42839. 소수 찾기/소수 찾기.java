08da37e06d32b5e55ebcedc6ae274ea664f606bf
Junbum2ya <141423549+JunBum2ya@users.noreply.github.com>
2024-06-07 10:35:37 +0900

    [level 2] Title: 소수 찾기, Time: 2.72 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,50 +1,48 @@
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
- 
+
 public class Solution {
-    static Set<Integer> set;
-    static boolean[] visited = new boolean[7]; // numbers는 길이 1 이상 7 이하인 문자열
 
     public int solution(String numbers) {
         int answer = 0;
-        set = new HashSet<>();
-        dfs(numbers, "", 0);
- 
-        for (Integer num : set) {
-            if (isPrime(num)) {
+        Set<Integer> set = new HashSet<>();
+        int[] cards = Arrays.stream(numbers.split("")).mapToInt(Integer::parseInt).toArray();
+        boolean[] visited = new boolean[cards.length];
+        dfs(set, visited, cards, 0, 0);
+        for(Integer number : set) {
+            if(isPrime(number)) {
                 answer++;
             }
         }
         return answer;
     }
- 
-    public static void dfs(String numbers, String s, int depth) {
-		// numbers 의 끝까지 다 탐색했다면 종료
-        if (depth > numbers.length()) {
+
+    private void dfs(Set<Integer> set, boolean[] visited, int[] cards, int result, int number) {
+        if(number > cards.length) {
             return;
         }
- 
-        for (int i = 0; i < numbers.length(); i++) {
+        for(int i = 0; i < cards.length; i++) {
             if(!visited[i]) {
                 visited[i] = true;
-                set.add(Integer.parseInt(s + numbers.charAt(i)));
-                dfs(numbers ,s + numbers.charAt(i), depth + 1);
+                int card = cards[i];
+                int temp = result * 10 + card;
+                set.add(temp);
+                dfs(set, visited, cards, temp, number + 1);
                 visited[i] = false;
             }
         }
     }
- 
-    public static boolean isPrime(int n) {
-        if (n < 2) {
+
+    private boolean isPrime(int number) {
+        if(number < 2) {
             return false;
         }
-		// 에라토스테네스 체
-        for (int i = 2; i <= (int) Math.sqrt(n); i++) {
-            if (n % i == 0) {
+        for(int i = 2; i <= (int) Math.sqrt(number); i++) {
+            if(number % i == 0) {
                 return false;
             }
         }
- 
         return true;
     }
 }
